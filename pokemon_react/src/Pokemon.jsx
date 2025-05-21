2d6b793d573aafe48b9213ec619d9b95081b2ed4
Arjun Arora <06arjunarora@gmail.com>
2025-05-21 14:09:10 +0530

    feat: Implement Pokemon React App with Vite
    This commit introduces the initial implementation of a Pokemon React application, built using Vite as the build tool.
    
    The changes include:
    
    - Creation of core React components (`App`, `Pokemon`, `PokemonCards`) to fetch and display Pokemon data from the PokeAPI.
    - Implementation of a search functionality to filter Pokemon based on user input.
    - Styling using CSS to create a visually appealing user interface.
    - Configuration of Vite for React development, including plugin setup.
    - Addition of necessary files such as `.gitignore`, `index.html`, `README.md`, `eslint.config.js`, `package.json`, and assets.
    
    This commit sets up the basic structure and functionality of the Pokemon React application, providing a foundation for future enhancements and features.

---
+++ pokemon_react/src/Pokemon.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import "./index.css"
+import { PokemonCards } from "./PokemonCards";
+
+
+export const Pokemon = () => {
+
+    const [pokemon, setPokemon] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [value, setValue] = useState("");
+
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=28";
+
+    const fetchPokemon = async() => {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+
+            const detailedPokemonData = data.results.map(async (currPokemon)=> {
+                const res = await fetch(currPokemon.url);
+                const data = await res.json();
+                return data;
+            });
+
+            const detailedResponses = await Promise.all(detailedPokemonData);
+            setPokemon(detailedResponses);
+            setLoading(false);
+
+        } catch (error) {
+            console.error(error.message);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchPokemon();
+    }, []);
+
+    const searchData = pokemon.filter((currPokemon) => currPokemon.name.toLowerCase().includes(value.toLowerCase()));
+
+    if(loading){
+        return(
+            <>
+                <div>
+                    <h1>Loading....</h1>
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <section className="container">
+                <header>
+                    <h1>Let's Catch Pokemon</h1>
+                </header>
+                <div className="pokemon-search">
+                    <input type="text" placeholder="Search Pokemon" value={value} onChange={(e) => setValue(e.target.value)} />
+                </div>
+                <div>
+                    <ul className="cards">
+                        {searchData.map((currPokemon) => {
+                            return(
+                                <PokemonCards key={currPokemon.id} pokemonData={currPokemon} />
+                            );
+                        })}
+                    </ul>
+                </div>
+            </section>
+        </>
+    );
+}
